54c588ed6813e7ccb84785517a791ae12ca3e34c
rentaro731 <horen731731@gmail.com>
2025-12-30 22:36:30 +0900

    プロフィール　アイコンイニシャル　入力欄初期値にユーザー名表示
    アイコンイニシャル表示
    入力欄初期値にユーザー名

--- src/sideBar/Setting.jsx
+++ src/sideBar/Setting.jsx
@@ -1,9 +1,54 @@
 import React from "react";
 
 export const Setting = () => {
+  const [name, setName] = useState("");
+  const { user, loading } = useUserContext();
+
+  useEffect(() => {
+    if (user?.name) {
+      setName(user.name);
+    }
+  }, [user]);
+  if (loading) return <div>loading...</div>;
+
   return (
-    <div>
-      <h1>こんにちは</h1>
+    <div style={{ width: "100%" }}>
+      <h2 style={{ textAlign: "center" }}>プロフィール設定画面</h2>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          marginTop: "8px",
+        }}
+      >
+        <div style={{ position: "relative" }}>
+          <Avatar name={name || "User"} size="80" round={true} />
+          <FaUserEdit
+            size={24}
+            style={{ position: "absolute", right: "-24px", top: "10px" }}
+          />
+        </div>
+      </div>
+      <div style={{ marginTop: "16px", textAlign: "center" }}>
+        <label>表示名</label>
+        <input
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          type="text"
+          placeholder="ニックネームを入力"
+          style={{ width: "80%", padding: "8px", marginTop: "8px" }}
+        />
+      </div>
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <button
+          style={{
+            marginTop: "8px",
+            padding: "8px 16px",
+          }}
+        >
+          保存
+        </button>
+      </div>
     </div>
   );
 };
