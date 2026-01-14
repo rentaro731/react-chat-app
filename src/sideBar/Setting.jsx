7c0f6db59a8653e33c08ed6ee0e21f04ba105573
rentaro731 <horen731731@gmail.com>
2026-01-14 13:46:50 +0900

    stting.cssの作成　インラインからmodule
    setting.jsxインラインスタイルからcss.module

--- src/sideBar/Setting.jsx
+++ src/sideBar/Setting.jsx
@@ -1,4 +1,8 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import Avatar from "react-avatar";
+import { FaUserEdit } from "react-icons/fa";
+import { useUserContext } from "../UserContext";
+import styles from "../css/setting.module.css";
 
 export const Setting = () => {
   const [name, setName] = useState("");
@@ -12,42 +16,26 @@ export const Setting = () => {
   if (loading) return <div>loading...</div>;
 
   return (
-    <div style={{ width: "100%" }}>
-      <h2 style={{ textAlign: "center" }}>プロフィール設定画面</h2>
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          marginTop: "8px",
-        }}
-      >
-        <div style={{ position: "relative" }}>
+    <div className={styles.main}>
+      <h2 className={styles.title}>プロフィール設定画面</h2>
+      <div className={styles.avatarContainer}>
+        <div className={styles.avatarWrapper}>
           <Avatar name={name || "User"} size="80" round={true} />
-          <FaUserEdit
-            size={24}
-            style={{ position: "absolute", right: "-24px", top: "10px" }}
-          />
+          <FaUserEdit size={24} className={styles.faUserEdit} />
         </div>
       </div>
-      <div style={{ marginTop: "16px", textAlign: "center" }}>
+      <div className={styles.inputContainer}>
         <label>表示名</label>
         <input
           value={name}
           onChange={(e) => setName(e.target.value)}
           type="text"
           placeholder="ニックネームを入力"
-          style={{ width: "80%", padding: "8px", marginTop: "8px" }}
+          className={styles.inputName}
         />
       </div>
-      <div style={{ display: "flex", justifyContent: "center" }}>
-        <button
-          style={{
-            marginTop: "8px",
-            padding: "8px 16px",
-          }}
-        >
-          保存
-        </button>
+      <div className={styles.buttonContainer}>
+        <button className={styles.saveButton}>保存</button>
       </div>
     </div>
   );
